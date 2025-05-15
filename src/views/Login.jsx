1fdfdf29d7fb3c7550445dd35e9753e267419ab5
Andri Satriawan <satria.citieng@gmail.com>
2025-05-15 12:18:13 +0700

    feat: change bg login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -64,10 +64,10 @@ const LoginV2 = ({ mode }) => {
   // Vars
   const darkImg = '/images/pages/auth-mask-dark.png'
   const lightImg = '/images/pages/auth-mask-light.png'
-  const darkIllustration = '/images/illustrations/auth/v2-login-dark.png'
-  const lightIllustration = '/images/illustrations/auth/v2-login-light.png'
-  const borderedDarkIllustration = '/images/illustrations/auth/v2-login-dark-border.png'
-  const borderedLightIllustration = '/images/illustrations/auth/v2-login-light-border.png'
+  const darkIllustration = '/images/illustrations/auth/login-bg.png'
+  const lightIllustration = '/images/illustrations/auth/login-bg.png'
+  const borderedDarkIllustration = '/images/illustrations/auth/login-bg.png'
+  const borderedLightIllustration = '/images/illustrations/auth/login-bg.png'
 
   // Hooks
   const router = useRouter()
